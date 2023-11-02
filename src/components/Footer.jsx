ed1598042ce8d3e30eb66f0b28c9d6a637b06e2c
Marian <hellomariandev@gmail.com>
2023-11-02 23:34:14 +0100

    simple footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+export default function Footer() {
+  return (
+    <footer
+      style={{
+        padding: "20px 20px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        backgroundColor: "black",
+        color: "whitesmoke",
+      }}
+    >
+      <h3>You can follow us on facebook, twitter and instagram.</h3>
+    </footer>
+  );
+}
